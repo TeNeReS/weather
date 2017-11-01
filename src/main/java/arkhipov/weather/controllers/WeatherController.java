7d2ebe1f3f58d00cf527a646fcf5f02c2020d971
Vladimir Arkhipov <vi.arhipov@yandex.ru>
2017-11-01 10:27:10 +0300

    Request-response logging added.

--- src/main/java/arkhipov/weather/controllers/WeatherController.java
+++ src/main/java/arkhipov/weather/controllers/WeatherController.java
@@ -2,6 +2,8 @@ package arkhipov.weather.controllers;
 
 import arkhipov.weather.models.Location;
 import arkhipov.weather.services.WeatherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
+    private static Logger LOG = LoggerFactory.getLogger(WeatherController.class);
     private WeatherService service;
     @Autowired
     public WeatherController(WeatherService service) {
@@ -18,11 +21,18 @@ public class WeatherController {
 
     @RequestMapping("coordinates")
     public Location getWeatherByCoordinates(@RequestParam(value = "lon") String lon, @RequestParam(value = "lat") String lat) {
-        return service.getWeatherByCoordinates(lon, lat);
+        Location respLocation = service.getWeatherByCoordinates(lon, lat);
+        LOG.info("Request with parameters {lon=" + lon +
+                ", lat=" + lat +
+                "} -> Response: " + respLocation);
+        return respLocation;
     }
 
     @RequestMapping("location")
     public Location getWeatherByName(@RequestParam(value = "name") String name) {
-        return service.getWeatherByName(name);
+        Location respLocation = service.getWeatherByName(name);
+        LOG.info("Request with parameters {name=" + name +
+                "} -> Response: " + respLocation);
+        return respLocation;
     }
 }
